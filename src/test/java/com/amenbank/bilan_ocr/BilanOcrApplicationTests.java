36f2d75288fd31319927c36428a84903d97a25ea
mohamineoueslati <mohamad.amine.oueslati@gmail.com>
2022-07-21 14:33:35 +0100

    creating project

---
+++ src/test/java/com/amenbank/bilan_ocr/BilanOcrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amenbank.bilan_ocr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilanOcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
