6597591ac53109da6dd5b14fb07bb161cb84ccf4
mohamineoueslati <amin.oueslati@etudiant-fst.utm.tn>
2022-10-24 12:40:30 +0100

    changing db props

--- src/test/java/com/amenbank/bilan_ocr/BilanOcrApplicationTests.java
+++ src/test/java/com/amenbank/bilan_ocr/BilanOcrApplicationTests.java
@@ -1,24 +1,50 @@
 package com.amenbank.bilan_ocr;
 
+import com.amenbank.bilan_ocr.entity.Role;
 import com.amenbank.bilan_ocr.entity.User;
-import com.amenbank.bilan_ocr.repository.UserRepository;
+import com.amenbank.bilan_ocr.repository.RoleRepository;
+import com.amenbank.bilan_ocr.service.IUserService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class BilanOcrApplicationTests {
     @Autowired
-    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    private IUserService userService;
 
     @Test
-    void test() {
-        var user = new User();
-        user.setUsername("moh");
-        user.setPassword("moh");
-        user.setFirstName("moh");
-        user.setLastName("moh");
+    void createRoles() {
+        var admin = new Role();
+        admin.setRole("ADMIN");
 
-        userRepository.save(user);
+        var user = new Role();
+        user.setRole("USER");
+
+        roleRepository.saveAll(List.of(admin, user));
+    }
+
+    @Test
+    void createAdmin() {
+        var adminRole = roleRepository.findByRole("ADMIN");
+
+        var user = new User();
+        user.setUsername("admin");
+        user.setPassword("admin");
+        user.setFirstName("admin");
+        user.setLastName("admin");
+        user.setEnabled(true);
+
+        Assertions.assertTrue(adminRole.isPresent());
+
+        user.setRole(adminRole.get());
+
+        userService.save(user);
     }
 }
